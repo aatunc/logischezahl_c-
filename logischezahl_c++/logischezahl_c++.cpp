3af42d1dadf36cee7e90ad1754e1e0738b4f6713
A Tunc <106016458+aatunc@users.noreply.github.com>
2023-03-13 13:26:38 +0100

    Projektdateien hinzufügen.

---
+++ logischezahl_c++/logischezahl_c++.cpp
@@ -0,0 +1,34 @@
+// logischezahl_c++.cpp : Diese Datei enthält die Funktion "main". Hier beginnt und endet die Ausführung des Programms.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int num1, num2, sum;
+
+    cout << "Bitte geben Sie die erste Zahl ein: ";
+    cin >> num1;
+
+    cout << "Bitte geben Sie die zweite Zahl ein: ";
+    cin >> num2;
+
+    sum = num1 + num2;
+
+    cout << "Die Summe von " << num1 << " und " << num2 << " ist " << sum << endl;
+
+    return 0;
+}
+
+
+// Programm ausführen: STRG+F5 oder Menüeintrag "Debuggen" > "Starten ohne Debuggen starten"
+// Programm debuggen: F5 oder "Debuggen" > Menü "Debuggen starten"
+
+// Tipps für den Einstieg: 
+//   1. Verwenden Sie das Projektmappen-Explorer-Fenster zum Hinzufügen/Verwalten von Dateien.
+//   2. Verwenden Sie das Team Explorer-Fenster zum Herstellen einer Verbindung mit der Quellcodeverwaltung.
+//   3. Verwenden Sie das Ausgabefenster, um die Buildausgabe und andere Nachrichten anzuzeigen.
+//   4. Verwenden Sie das Fenster "Fehlerliste", um Fehler anzuzeigen.
+//   5. Wechseln Sie zu "Projekt" > "Neues Element hinzufügen", um neue Codedateien zu erstellen, bzw. zu "Projekt" > "Vorhandenes Element hinzufügen", um dem Projekt vorhandene Codedateien hinzuzufügen.
+//   6. Um dieses Projekt später erneut zu öffnen, wechseln Sie zu "Datei" > "Öffnen" > "Projekt", und wählen Sie die SLN-Datei aus.
